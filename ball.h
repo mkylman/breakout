0fb1f0b895b230cf61ec00285149ad02f66608ae
mkylman <66576922+mkylman@users.noreply.github.com>
2021-09-23 12:17:03 -0400

    Update ball.h

--- ball.h
+++ ball.h
@@ -4,7 +4,7 @@ typedef struct {
   uint16_t color;
   int16_t  x, y;
   uint16_t oldx, oldy;
-  int8_t   xspeed, yspeed;
+  float    xspeed, yspeed;
   uint8_t  d; // diameter
   int8_t   lives;
 } Ball;
@@ -18,12 +18,42 @@ void drawBall(Ball *ball) {
                   ball->color );
 }
 
+void launchBall( Ball *ball ) {
+  delay(200);
+  Point p = {0,false};
+  while ( !p.touched ) {
+    p = getPoint();
+    if ( p.touched ) {
+      double x1, x2, y1, y2;
+      double a, b, c;
+      
+      x1 = p.x;
+      y1 = p.y;
+      while ( p.touched ) {
+        a = p.x - x1;
+        b = p.y - y1;
+        c = sqrt( (a*a) + (b*b) );
+        c = c ? c : 0.1;
+        
+        x2 = ball->x + (a / c) * 100;
+        y2 = ball->y + (b / c) * 100;
+        
+        tft.drawLine( ball->x, ball->y, x2, y2, WHITE );
+        tft.drawLine( ball->x, ball->y, x2, y2, BLACK );
+        
+        p = getPoint();
+      }
+      
+      ball->xspeed = a / c * 4;
+      ball->yspeed = b / c * 4;
+      if (ball->xspeed && ball->yspeed) break;
+    }
+  }
+}
+
 void ballMove(Ball *ball) {
   if (!ball->xspeed && !ball->yspeed) { // if ball is not moving
-    if ( touched() ) {                   // if touch, launch ball
-      ball->xspeed = getXP() > WIDTH / 2 ? XSPEED : -XSPEED;
-      ball->yspeed = -YSPEED;
-    }
+    launchBall( ball );
   }
   ball->oldx = ball->x;
   ball->oldy = ball->y;
@@ -44,7 +74,7 @@ uint8_t edgeCollision(Ball *ball) { // returns 0 if die, 1 if regular collision
   if (ball->y <= ball->d) {         // top side
     ball->y += ball->d;
     ball->yspeed *= -1;
-    ball->xspeed = ball->xspeed ? ball->xspeed : XSPEED;
+    //ball->xspeed = ball->xspeed ? ball->xspeed : XSPEED;
   }
   if (ball->y > HEIGHT - ball->d) { // if ball hits bottom, reset
     ball->yspeed = 0;
@@ -57,21 +87,21 @@ uint8_t edgeCollision(Ball *ball) { // returns 0 if die, 1 if regular collision
 
 void paddleCollision(Ball *ball, Paddle *paddle) {
   // Paddle collision
+  double a = ball->x - paddle->x + (paddle->w/2);
+  double b = paddle->y + (paddle->h/2) - ball->y;
+  float  c = sqrt( (a*a) + (b*b) );
+  c = c ? c : 0.1;
   if (ball->x < paddle->x + paddle->w && ball->x           > paddle->x
   &&  ball->y < paddle->y + paddle->h && ball->y + ball->d > paddle->y) {
-    ball->y -= ball->d;
-    ball->yspeed = -YSPEED;
-    uint8_t pthird = paddle->w / 3;
-    if (ball->xspeed == 0) {
-      ball->xspeed = -XSPEED;
-    }
-    if ((ball->x < paddle->x + pthird   && ball->xspeed > 0) //left side
-    ||  (ball->x > paddle->x + pthird*2 && ball->xspeed < 0)) {//right side
-      ball->xspeed *= -1;
-    } else if (ball->x > paddle->x + pthird
-            && ball->x < paddle->x + pthird*2) { //center
-      ball->xspeed = 0;
-    }
+    if (ball->xspeed > 0)
+      ball->xspeed = a / c * 4;
+    else
+      ball->xspeed = (a / c) * -4;
+    ball->yspeed = b / c * 4;
+    ball->yspeed *= -1;
+    
+    if (ball->yspeed == 0)
+      ball->yspeed = -1;
   }
 }
 
@@ -95,6 +125,7 @@ bool brickCollision(Ball *ball, Brick *brick){
       ball->xspeed = ball->xspeed ? ball->xspeed : (millis() % 2 ? XSPEED : -XSPEED);
       ball->yspeed *= -1;
     }
+    
     return true;
   } else return false;
 }
